e9a45d485c1fcd9778efdf6bd2588d53bef03b3e
Mostafa Salah <msalah_andela@medable.com>
2021-12-17 04:06:06 +0200

    fixed icon component

--- components/Header.js
+++ components/Header.js
@@ -12,14 +12,14 @@ import HeaderItem from "./HeaderItem";
 
 export default function Header() {
     return (
-        <header className="">
-            <div>
-                <HeaderItem title='HOME' Icon={HomeIcon} />
-                <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
-                <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
-                <HeaderItem title='COLLECTIONS' Icon={CollectionIcon} />
-                <HeaderItem title='SEARCH' Icon={SearchIcon} />
-                <HeaderItem title='ACCOUNT' Icon={UserIcon} />
+        <header className="flex flex-row justify-between">
+            <div className="flex flex-row justify-around">
+                <HeaderItem title='HOME' Icon={<HomeIcon width={50}  className="text-xs h-2 mb-1 group-hover:animate-bounce"/>} />
+                <HeaderItem title='TRENDING' Icon={<LightningBoltIcon width={50} className="h-2 mb-1 group-hover:animate-bounce"/>} />
+                <HeaderItem title='VERIFIED' Icon={<BadgeCheckIcon width={50} className="h-2 mb-1 group-hover:animate-bounce"/>} />
+                <HeaderItem title='COLLECTIONS' Icon={<CollectionIcon width={50} className="h-2 mb-1 group-hover:animate-bounce"/>} />
+                <HeaderItem title='SEARCH' Icon={<SearchIcon width={50} className="h-2 mb-1 group-hover:animate-bounce"/>} />
+                <HeaderItem title='ACCOUNT' Icon={<UserIcon width={50} className="h-8 mb-1 group-hover:animate-bounce"/>} />
             </div>
             <Image 
             className="object-contained"
